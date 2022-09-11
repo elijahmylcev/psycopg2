e796c581d95eebefce542e5c9d96df45c28f57ff
Elijah Mylcev <elijahmylcev@gmail.com>
2022-09-11 20:06:59 +0300

    Crate table; Insert INTO a table

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from multiprocessing import connection
 import psycopg2
 from config import host, user, password, db_name
 
+# connection.autocommit = True
+
 try:
   # connect to exist database
   connection = psycopg2.connect(
@@ -18,6 +20,27 @@ try:
     )
 
     print(f'Server version: {cursor.fetchone()}')
+
+  # create new table
+  # with connection.cursor() as cursor:
+  #   cursor.execute(
+  #     """CREATE TABLE users(
+  #       id serial PRIMARY KEY,
+  #       first_name varchar(50) NOT NULL,
+  #       nick_name varchar(50) NOT NULL);"""
+  #   )
+  #   connection.commit()
+  #   print("[INFO] Table created successfully")
+
+  # insert 
+  with connection.cursor() as cursor:
+    cursor.execute(
+      """INSERT INTO users(first_name, nick_name) VALUES ('Ily', 'Elijah');"""
+    )
+    connection.commit()
+    print("[INFO] Data was successfully inserted")
+  
+  # Get data from a table
 
 except Exception as _ex:
   print('[INFO] Error while with PostgreSQL', _ex)
