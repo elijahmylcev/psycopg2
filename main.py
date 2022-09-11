432408466fbfa62ecc3b17d41df43a1efb456233
Elijah Mylcev <elijahmylcev@gmail.com>
2022-09-11 19:45:52 +0300

    success connect to db

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+from multiprocessing import connection
 import psycopg2
-from config import host, user, password, db_name,port
+from config import host, user, password, db_name
 
+try:
+  # connect to exist database
+  connection = psycopg2.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=db_name
+  )
+  # the cursor for performing database operations
+
+  with connection.cursor() as cursor:
+    cursor.execute(
+      "SELECT version();"
+    )
+
+    print(f'Server version: {cursor.fetchone()}')
+
+except Exception as _ex:
+  print('[INFO] Error while with PostgreSQL', _ex)
+finally:
+  if connection:
+    connection.close()
+    print('[INFO] PostgreSQL connection closed')
